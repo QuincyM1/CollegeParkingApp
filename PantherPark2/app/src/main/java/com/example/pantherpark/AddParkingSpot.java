ad935f8e5472d68e549b018aa1be041dd4b410bd
Kris <kris.small24@gmail.com>
2023-04-18 20:31:55 -0400

    Added user location grab under park

--- PantherPark2/app/src/main/java/com/example/pantherpark/AddParkingSpot.java
+++ PantherPark2/app/src/main/java/com/example/pantherpark/AddParkingSpot.java
@@ -3,12 +3,15 @@ package com.example.pantherpark;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 import com.example.pantherpark.data_objects.AdminUser;
 
 public class AddParkingSpot extends AppCompatActivity {
 
     private EditText etSpotNumber, etParkingLotId, etReservationStatus;
+    private TextView ReservationStatus;
 
     // Create an instance of the AdminUser class
     private AdminUser adminUser = new AdminUser();
@@ -17,7 +20,7 @@ public class AddParkingSpot extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_parking_spot);
-
+        ReservationStatus = findViewById(R.id.reservationStatus);
         etSpotNumber = findViewById(R.id.spot_code_input);
         etParkingLotId = findViewById(R.id.deck_id_input);
         etReservationStatus = findViewById(R.id.reservation_status_input);
--- PantherPark2/app/src/main/java/com/example/pantherpark/Park.java
+++ PantherPark2/app/src/main/java/com/example/pantherpark/Park.java
@@ -2,8 +2,13 @@ package com.example.pantherpark;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
@@ -12,17 +17,20 @@ import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import com.google.android.gms.*;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.*;
 
-
+import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
 
-public class Park extends AppCompatActivity implements OnMapReadyCallback, AdapterView.OnItemSelectedListener {
+public class Park extends AppCompatActivity implements OnMapReadyCallback, AdapterView.OnItemSelectedListener, LocationListener {
 
     /* ToDo: add onClick instances for existing and future buttons/lists,
         add responses to bottom nav options (Account, Park, Reservation),
@@ -33,13 +41,19 @@ public class Park extends AppCompatActivity implements OnMapReadyCallback, Adapt
     BottomNavigationView bottomNavigationView;
     boolean isPermissionGranted;
 
-    LatLng selection = new LatLng(0,0);
+    LatLng selection = new LatLng(0, 0);
+
+    LatLng InitialPosition = new LatLng(0, 0);
 
     GoogleMap mMap;
+
+    FusedLocationProviderClient f;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        f = LocationServices.getFusedLocationProviderClient(this);
         setContentView(R.layout.fragment_park);
         if (getSupportActionBar() != null) {
             getSupportActionBar().hide();
@@ -50,12 +64,31 @@ public class Park extends AppCompatActivity implements OnMapReadyCallback, Adapt
         SupportMapFragment mapFragment = SupportMapFragment.newInstance();
         getSupportFragmentManager().beginTransaction().replace(R.id.map, mapFragment).commit();
         mapFragment.getMapAsync(this);
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        f.getLastLocation().addOnSuccessListener(this, new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+                if (location != null) {
+                    LatLng ll = new LatLng(location.getLatitude(), location.getLongitude());
+                    makePosition(mMap, ll, "you");
+                }
+            }
+        });
         //set up bottom navigation
         bottomNavigationView = findViewById(R.id.bottomNavigationView2);
         bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(MenuItem item) {
-                switch(item.getItemId()) {
+                switch (item.getItemId()) {
                     case R.id.parking:
                         spinner.setVisibility(View.VISIBLE);
                         //Parking park = new Parking();
@@ -63,7 +96,7 @@ public class Park extends AppCompatActivity implements OnMapReadyCallback, Adapt
                         return true;
                     case R.id.account:
                         bottomNavigationView.setSelectedItemId(R.id.parking);
-                        spinner.setVisibility(View.GONE);
+                        spinner.setVisibility(View.VISIBLE);
                         Intent intent = new Intent(Park.this, AccountScreen.class);
                         startActivity(intent);
 
@@ -90,6 +123,7 @@ public class Park extends AppCompatActivity implements OnMapReadyCallback, Adapt
                     clearMarker(mMap);
                     Toast.makeText(adapter.getContext(), "Please Select a Destination", Toast.LENGTH_SHORT).show();
                     resetPosition(mMap);
+                    makePosition(mMap, InitialPosition, "rada");
                 }
                 if (adapterView.getItemAtPosition(i).equals("B Deck")) {
                     clearMarker(mMap);
@@ -115,7 +149,6 @@ public class Park extends AppCompatActivity implements OnMapReadyCallback, Adapt
     }
 
 
-
     @Override
     public void onMapReady(@NonNull GoogleMap googleMap) {
         LatLng InitialPosition = new LatLng(33.7488, -84.3877);
@@ -138,10 +171,29 @@ public class Park extends AppCompatActivity implements OnMapReadyCallback, Adapt
             }
         });
         //googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(selection, 18));
-        googleMap.addMarker(new MarkerOptions().position(selection).title(s));
+        googleMap.addMarker(new MarkerOptions().position(LL).title(s));
     }
-    public void resetPosition(@NonNull GoogleMap googleMap){
-        LatLng InitialPosition = new LatLng(33.7488, -84.3877);
+
+    public void resetPosition(@NonNull GoogleMap googleMap) {
+        //LatLng InitialPosition = new LatLng(33.7488, -84.3877);
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        f.getLastLocation().addOnSuccessListener(this, new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+                if (location != null) {
+                    InitialPosition = new LatLng(location.getLatitude(), location.getLongitude());
+                }
+            }
+        });
         CameraPosition cameraPosition = new CameraPosition.Builder().target(InitialPosition).zoom(14).build();
         googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition), 1500, new GoogleMap.CancelableCallback() {
             @Override
@@ -166,6 +218,11 @@ public class Park extends AppCompatActivity implements OnMapReadyCallback, Adapt
     }
     @Override
     public void onNothingSelected(AdapterView<?> adapterView) {
+
+    }
+
+    @Override
+    public void onLocationChanged(@NonNull Location location) {
 
     }
 }
