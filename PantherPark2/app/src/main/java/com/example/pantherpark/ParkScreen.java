bba750c4983ab6d77b8411baafd6f16e003d362a
aphoticSunshine <kris.small24@GMAIL.com>
2023-04-23 12:35:05 -0400

    Changed onready check for user location and put it into OnMapReady function in ParkScreen.java

--- PantherPark2/app/src/main/java/com/example/pantherpark/ParkScreen.java
+++ PantherPark2/app/src/main/java/com/example/pantherpark/ParkScreen.java
@@ -4,6 +4,8 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -13,13 +15,22 @@ import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.Toast;
 
-import com.google.android.gms.maps.*;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
-
+import com.example.pantherpark.dbinterface.DBManager;
+import com.example.pantherpark.dbinterface.DeckData;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
 
@@ -28,14 +39,17 @@ public class ParkScreen extends AppCompatActivity implements OnMapReadyCallback,
     ArrayAdapter<CharSequence> adapter;
     BottomNavigationView bottomNavigationView;
 
+    FusedLocationProviderClient f;
     LatLng selection = new LatLng(0, 0);
 
+    LatLng InitialPosition = new LatLng(0,0);
     GoogleMap mMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
+        f = LocationServices.getFusedLocationProviderClient(this);
         setContentView(R.layout.activity_park_screen);
         if (getSupportActionBar() != null) {
             getSupportActionBar().hide();
@@ -46,7 +60,6 @@ public class ParkScreen extends AppCompatActivity implements OnMapReadyCallback,
         SupportMapFragment mapFragment = SupportMapFragment.newInstance();
         getSupportFragmentManager().beginTransaction().replace(R.id.map, mapFragment).commit();
         mapFragment.getMapAsync(this);
-
 
         //set up bottom navigation
         bottomNavigationView = findViewById(R.id.bottomNavigationView2);
@@ -61,7 +74,7 @@ public class ParkScreen extends AppCompatActivity implements OnMapReadyCallback,
 
                         Intent intent = new Intent(ParkScreen.this, AccountScreen.class);
                         startActivity(intent);
-                        return false;
+                        return true;
                 }
                 return false;
             }
@@ -76,18 +89,26 @@ public class ParkScreen extends AppCompatActivity implements OnMapReadyCallback,
         spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                DBManager dbmg = new DBManager();
+                DeckData deck = dbmg.getDeckInformation("A Deck");
                 // if user selects [Select A Destination]
                 if (adapterView.getItemAtPosition(i).equals("Select a Destination")) {
                     clearMarker(mMap);
                     Toast.makeText(adapter.getContext(), "Please Select a Destination", Toast.LENGTH_SHORT).show();
                     resetPosition(mMap);
                 }
-                if (adapterView.getItemAtPosition(i).equals("B Deck")) {
+                else {
+                    clearMarker(mMap);
+                    selection = deck.getLatLng();
+                    makePosition(mMap, selection, adapterView.getItemAtPosition(i).toString());
+                }
+
+                /*if (adapterView.getItemAtPosition(i).equals("B Deck")) {
                     clearMarker(mMap);
                     selection = new LatLng(33.7521941, -84.3856621);
                     makePosition(mMap, selection, adapterView.getItemAtPosition(i).toString());
 
-                }
+                }*/
                 //makePosition(mMap, getDeckLatLng(adapterView.getItemAtPosition(i).toString()), adapterView.getItemAtPosition(i).toString());
 
 
@@ -110,6 +131,24 @@ public class ParkScreen extends AppCompatActivity implements OnMapReadyCallback,
         LatLng InitialPosition = new LatLng(33.7488, -84.3877);
         googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(InitialPosition, 14));
         mMap = googleMap;
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        f.getLastLocation().addOnSuccessListener(this, new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+                if (location != null){
+                    LatLng InitialPosition = new LatLng(location.getLatitude(), location.getLongitude());
+                }
+            }
+        });
     }
 
 
@@ -131,7 +170,7 @@ public class ParkScreen extends AppCompatActivity implements OnMapReadyCallback,
     }
 
     public void resetPosition(@NonNull GoogleMap googleMap) {
-        LatLng InitialPosition = new LatLng(33.7488, -84.3877);
+        //LatLng InitialPosition = new LatLng(33.7488, -84.3877);
         CameraPosition cameraPosition = new CameraPosition.Builder().target(InitialPosition).zoom(14).build();
         googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition), 1500, new GoogleMap.CancelableCallback() {
             @Override
@@ -160,12 +199,4 @@ public class ParkScreen extends AppCompatActivity implements OnMapReadyCallback,
     public void onNothingSelected(AdapterView<?> adapterView) {
 
     }
-
-    public static void setCheckable(BottomNavigationView view, boolean checkable) {
-        final Menu menu = view.getMenu();
-        for (int i = 0; i < menu.size(); i++) {
-            menu.getItem(i).setCheckable(checkable);
-        }
-    }
-
 }
